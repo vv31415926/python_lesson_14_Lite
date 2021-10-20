7e55f2fd6dbb6911c09d5abba8b555f85d7aff64
vv31415926 <valery31415926@mail.ru>
2021-10-20 21:05:06 +0500

    1

---
+++ main.py
@@ -0,0 +1,79 @@
+import requests
+import pprint
+import csv
+from bs4 import BeautifulSoup
+
+domain = 'https://chelyabinsk.n1.ru'
+URL = f'{domain}/kupit/kvartiry/vtorichka/district-Kalininskiy-rayon/'
+
+response = requests.get( URL )
+print( response.status_code )
+
+soup = BeautifulSoup( response.text, 'html.parser')
+
+div_tags = soup.find_all( 'div', class_='living-list-card__col _main' )
+#print( len( div_tags ), type( div_tags ))
+
+lst = []
+for v1 in div_tags:
+    #print( v1 )
+
+    dic = {}
+    dic['characteristic'] = ''
+
+    try:
+        tags = v1.find('a', class_='link')
+        #print(tags.text)
+        dic['address'] = tags.text
+    except  AttributeError:
+        pass
+    try:
+        tags = v1.find('div', class_='search-item-district living-list-card__inner-block')
+        #print(  tags.text )
+        dic['region'] = tags.text
+    except  AttributeError:
+        pass
+
+    try:
+        tags = v1.find('div', class_='living-list-card__city-with-estate living-list-card-city-with-estate living-list-card__inner-block')
+        #print(tags.text)
+        dic['city'] = tags.text
+    except  AttributeError:
+        pass
+
+    try:
+        tags = v1.find('div', class_='living-list-card__area living-list-card-area living-list-card__inner-block')
+        #print(tags.text)
+        dic['characteristic'] += tags.text
+    except  AttributeError:
+        pass
+
+    try:
+        tags = v1.find('div', class_='living-list-card__floor living-list-card-floor living-list-card__inner-block')
+        #print(tags.text)
+        dic['characteristic'] += " "+tags.text
+    except  AttributeError:
+        pass
+
+    try:
+        tags = v1.find('div', class_='living-list-card__material living-list-card-material living-list-card__inner-block')
+        #print(tags.text)
+        dic['characteristic'] += " " + tags.text
+    except  AttributeError:
+        pass
+
+    lst.append( dic )
+    #print('-----------')
+
+print("\n=================================")
+pprint.pprint( lst )
+print("=================================\n")
+
+title =  ['region', 'city','address','characteristic']
+
+with open( "chel_hausing.csv", mode="w", encoding='utf-8', newline='') as w_file:
+    f = csv.writer(w_file, delimiter = "!" )
+    f.writerow( title )
+    for d in lst:
+        #print( d['region'], d['city'], d['address'], d['characteristic'] )
+        f.writerow(  [ d['region'], d['city'], d['address'], d['characteristic']  ]    )
